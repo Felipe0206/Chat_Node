091209cc1729a552180d83ff6c2f09c9fc66a87b
unknown <conocimiento.02@gmail.com>
2021-10-26 15:09:01 -0500

    Actualizacion Andres

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -21,9 +21,9 @@ export default function Login() {
         <div className="login">
             <div className="loginWrapper">
                 <div className="loginLeft">
-                    <h3 className="loginLogo">Lamasocial</h3>
+                    <h3 className="loginLogo">Intopcol</h3>
                     <span className="loginDesc">
-                        Connect with friends and the world around you on Lamasocial.
+                        Chat corporativo de Intopcol
                     </span>
                 </div>
                 <div className="loginRight">
