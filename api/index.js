e91db89c77143d0c70bdeae9512e78e416cab5ca
Guillermo Garcia Soto <gasogui@gmail.com>
2021-10-26 22:08:46 -0500

    confi socket

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const http = require("http");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const multer = require("multer");
+
 // routes
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
@@ -48,12 +50,81 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
   }
 });
 
+/** Get port from environment and store in Express. */
+const port = process.env.PORT || "8800";
+app.set("port", port);
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/conversations", conversationRoute);
 app.use("/api/messages", messageRoute);
 
-app.listen(8800, () => {
-  console.log("Backend server is running!");
+/** catch 404 and forward to error handler */
+app.use("*", (req, res) => {
+  return res.status(404).json({
+    success: false,
+    message: "API endpoint doesnt exist",
+  });
+});
+
+/** Create HTTP server. */
+const server = http.createServer(app);
+/** Create socket connection */
+const io = require("socket.io")(server, {
+  cors: {
+    origin: true,
+    methods: ["GET", "POST"],
+    //credentials: true,
+  },
+});
+
+//io.on("connection", WebSockets.connection);
+/** Listen on provided port, on all network interfaces. */
+server.listen(port);
+/** Event listener for HTTP server "listening" event. */
+server.on("listening", () => {
+  console.log(`Listening on port:: http://localhost:${port}/`);
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  //when ceonnect
+  console.log("a user connected.");
+
+  //take userId and socketId from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  //send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
 });
